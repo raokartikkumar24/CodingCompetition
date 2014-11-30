2153a9137086f57e8def93a6ce331e027e7b71fa
Kartikkumar <krao24@gmail.com>
2014-11-30 11:06:15 +0530

    Combinatronics : Minimum sock draws

---
+++ Hackerrank/min_draws_socks.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+typedef long long int  LL;
+int main()
+{
+
+	LL tt;
+	cin>>tt;
+	while( tt --)
+	{
+		LL number;
+		cin>>number;
+		cout<<number+1<<endl;
+
+	}
+
+
+	return 0;
+
+}
