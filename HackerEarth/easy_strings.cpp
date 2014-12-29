890373fa1b50f8eceeda5d553596d9f0a538483b
Kartikkumar <krao24@gmail.com>
2014-12-29 21:15:24 +0530

    easy strings

--- HackerEarth/easy_strings.cpp
+++ HackerEarth/easy_strings.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main()
 {
-	int dashes[] = {6,2,5,5,4,5,6,4,7,6};
+	int dashes[] = {6,2,5,5,4,5,6,3,7,6};
 
 	string input;
 	cin>>input;
@@ -13,9 +13,7 @@ int main()
 	int length = input.length();
 	long long int sum = 0;
 	for (int i = 0; i < length; ++i)
-	{
 		sum += dashes[input[i] -'0'];
-	}
 
 	cout<<sum;
 
