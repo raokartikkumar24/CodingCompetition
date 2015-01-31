321bdff3ab1e210defdd15e111eff0d63e6e74f0
Kartikkumar <krao24@gmail.com>
2015-01-31 19:04:46 +0530

    summation of primes

---
+++ Hackerrank/euler_sum_prime.py
@@ -0,0 +1,32 @@
+#import timeit
+def sieve(n):
+  sieve = [True] * (n+1)
+  sieve[0] = sieve[1] = False
+  i = 2
+  primenumber = []
+  while (i*i <= n):
+    if (sieve[i]):
+      k = i*i
+      while (k <= n):
+        sieve[k] = False
+        k+=i #factors of the number
+    i+=1
+
+  for j in range(2,n+1):
+    if sieve[j]:
+      primenumber.append(j)
+
+  return primenumber
+
+
+if __name__ =="__main__":
+  primenumbers = sieve(1000000) #generate primenumbers
+  T = int(input())
+  while T > 0:
+    N = int(input())
+    primsum = 0
+    for p in range(0,N):
+      if primenumbers[p] <= N:
+        primsum += primenumbers[p]   
+    print(primsum)
+    T -= 1    
