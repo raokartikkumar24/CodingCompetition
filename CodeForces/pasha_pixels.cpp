d6ab3e1dc5f7761dd9bf864e5128cf4310879619
Kartikkumar <krao24@gmail.com>
2015-02-04 20:47:02 +0530

    final accepted solution

--- CodeForces/pasha_pixels.cpp
+++ CodeForces/pasha_pixels.cpp
@@ -14,34 +14,6 @@ int main()
 	vector<vector<int> > mat(n+2,vector<int> (m+2));
 
 
-	for (int i = 0; i < n+2; ++i)
-		mat[i][0] = -4;
-	
-
-
-	for (int i = 0; i < n+2; ++i)
-		mat[i][m+1] = -4;
-	
-
-	for (int i = 0; i < m+2; ++i)
-		mat[0][i] = -4;
-	
-
-	for (int i = 0; i < m+2; ++i)
-		mat[n+1][i] = -4;
-	
-
-
-	/*for (int i = 0; i < n+2; ++i)
-	{
-		for (int j = 0; j < m+2; ++j)
-		{
-			cout<<mat[i][j]<<"\t";
-		}
-		cout<<endl;
-	}*/
-
-
 	long temp = k;
 	long count = 0;
 	long ans = 0;
@@ -53,10 +25,12 @@ int main()
 
 		mat[p][q] = 1;
 
-		if((mat[p-1][q-1] + mat[p-1][q] + mat[p][q-1] + mat[p][q] == 4) ||
-		  (mat[p][q] + mat[p][q+1] + mat[p-1][q] + mat[p+1][q+1] == 4) ||
-		  (mat[p][q] + mat[p][q-1] + mat[p+1][q] + mat[p+1][q-1] == 4) ||
-		  (mat[p][q] + mat[p][q+1] + mat[p+1][q] + mat[p+1][q+1] == 4))
+
+
+		if( (mat[p][q] + mat[p-1][q-1] + mat[p-1][q] + mat[p][q-1] == 4) ||
+			(mat[p][q] + mat[p-1][q] + mat[p][q+1] + mat[p-1][q+1] == 4) ||
+			(mat[p][q] + mat[p][q+1] + mat[p+1][q] + mat[p+1][q+1] == 4) ||
+			(mat[p][q] + mat[p][q-1] + mat[p+1][q-1] + mat[p+1][q] == 4))
 		{
 			if(ans == 0)
 				ans = count;
