3eb9f291386abcf4a02737f857f6f4840fc240bd
Kartikkumar <krao24@gmail.com>
2015-02-09 11:42:27 +0530

    Inserion sort part 2

---
+++ Hackerrank/insertion_sort_2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin>>num;
+	int *ele = new int[num];
+	for(int k = 0 ; k < num ;k++)
+		cin>>ele[k];
+
+
+	int i,j,insertedElement;
+	
+
+	for(i = 0 ; i < num ; i++)
+	{
+		insertedElement = ele[i];
+		j = i;
+		while( ele[j-1] > insertedElement && j >= 1 )
+		{
+			//keep on moving the element until we find the correct position for that element.
+			ele[j] = ele[j-1];j--;
+					
+		}
+
+
+		ele[j] = insertedElement;
+
+		if( i != 0 ) {
+			for (int idx = 0; idx < num; ++idx)
+				cout<<ele[idx]<<" ";
+		cout<<endl;
+		}
+		
+
+		
+}
+
+	
+	return 0;
+}
