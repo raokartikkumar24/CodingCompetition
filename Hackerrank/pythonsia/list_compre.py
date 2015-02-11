d206351beb885895873fe32a709de6a285b303ec
Kartikkumar <krao24@gmail.com>
2015-02-11 15:01:13 +0530

    improved version. Less LOC

--- Hackerrank/pythonsia/list_compre.py
+++ Hackerrank/pythonsia/list_compre.py
@@ -3,17 +3,19 @@ Y = int(input())
 Z = int(input())
 N = int(input())
 
-
-
-xi = [i for i in range(0,X+1)]
-yi = [i for i in range(0,Y+1)]
-zi = [i for i in range(0,Z+1)]
-
-
-
-lip = [[x,y,z] for x in xi for y in yi for z in zi if x+y+z != N]
-
+lip = [[x,y,z] for x in range(0,X+1) for y in range(0,Y+1) for z in range(0,Z+1) if x+y+z != N]
 print(lip)
+
+#xi = [i for i in range(0,X+1)]
+#yi = [i for i in range(0,Y+1)]
+#zi = [i for i in range(0,Z+1)]
+
+
+
+#lip = [[x,y,z] for x in xi for y in yi for z in zi if x+y+z != N]
+
+
+
 
 
 
