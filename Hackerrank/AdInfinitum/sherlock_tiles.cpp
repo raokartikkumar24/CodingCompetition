241608901a163dcaebf9548328d92178694425b0
Kartikkumar <krao24@gmail.com>
2015-02-20 21:55:16 +0530

    ad

---
+++ Hackerrank/AdInfinitum/sherlock_tiles.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+typedef long long LL;
+using namespace std;
+
+int main()
+{
+
+	LL L,S1,S2;
+	cin>>L>>S1>>S2;
+	LL Q;
+	cin>>Q;
+	while( Q-- )
+	{
+
+		LL q;
+		cin>>q;
+
+		double result = sqrt(2*L*L)-sqrt(2*q);
+		printf("%2f",result);
+
+	}
+
+
+	return 0;
+}
+
