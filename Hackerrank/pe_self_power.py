dc46c102796ca9bfa8c5c2c403425d67298d2ba9
Kartikkumar <krao24@gmail.com>
2015-02-23 11:57:06 +0530

    Adding the inbuilt pow funtion with MOD parameter.

--- Hackerrank/pe_self_power.py
+++ Hackerrank/pe_self_power.py
@@ -1,8 +1,8 @@
 N = (int)(input())
-mod = 10**10
+mod = 10**10 #obtain last 10 digits
 prod = 0
 for i in range(1,N+1):
-	prod += i**i
+	prod += pow(i,i,MOD) 
 strr = str(prod)
 str2 = strr[-10:]
 if str2[0] == '0':
