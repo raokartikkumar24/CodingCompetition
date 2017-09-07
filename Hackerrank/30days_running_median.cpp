41f3d37cb1adb03a5c76f948be4d00d8c1faae39
Kartikkumar <krao24@gmail.com>
2017-09-07 11:57:19 +0530

    add pop when rebalancing, older cpp compilers doesnt support emplace and other changes

--- Hackerrank/30days_running_median.cpp
+++ Hackerrank/30days_running_median.cpp
@@ -7,33 +7,37 @@
 using namespace std;
 
 
-void addNumber(int number, priority_queue<int> &lowers, priority_queue<int,std::vector<int>, std::greater<int>> &highers){
+void addNumber(int number, priority_queue<int> &lowers, priority_queue<int,std::vector<int>, std::greater<int> > &highers){
     if(lowers.empty() || number < lowers.top())
-        lowers.emplace(number);
+        lowers.push(number);
     else
-        highers.emplace(number);
+        highers.push(number);
 }
 
-void rebalance(priority_queue<int> &lowers, priority_queue<int,std::vector<int>, std::greater<int>> &highers){
+void rebalance(priority_queue<int> &lowers, priority_queue<int,std::vector<int>, std::greater<int> > &highers){
     int lowerLength = lowers.size();
     int higherLength = highers.size();
   
     if(abs(lowerLength-higherLength) >= 2){
         if(lowerLength < higherLength){
-            lowers.emplace(highers.top());
+            lowers.push(highers.top());
+	    highers.pop();
         }
         else {
-            highers.emplace(lowers.top());
+            highers.push(lowers.top());
+            lowers.pop();
         }
     }
 }
 
-double getMedian(priority_queue<int> &lowers, priority_queue<int,std::vector<int>, std::greater<int>> &highers){
+double getMedian(priority_queue<int> &lowers, priority_queue<int,std::vector<int>, std::greater<int> > &highers){
      int lowerLength = lowers.size();
      int higherLength = highers.size();
         
      if(lowerLength == higherLength){
-         return (double)((lowers.top() + highers.top()) / 2);
+	int sum = lowers.top() + highers.top();
+	double median = (double)sum/2;
+	return median;
      }
     else {
         if(lowerLength > higherLength)
@@ -48,7 +52,7 @@ int main(){
     cin >> n;
     vector<int> a(n);
     priority_queue<int> lowers; //root is the max of all elements
-    priority_queue<int, std::vector<int>, std::greater<int>> highers; //root is min of all the elements
+    priority_queue<int, std::vector<int>, std::greater<int> > highers; //root is min of all the elements
     double median = 0.0;
     for(int a_i = 0;a_i < n;a_i++){
          cin >> a[a_i];
@@ -56,8 +60,7 @@ int main(){
         addNumber(a[a_i],lowers,highers);
         rebalance(lowers,highers);
         median = getMedian(lowers,highers);
-        
-        cout << median << endl;
+      	printf("%.1f \n",median);  
     }
     
     
