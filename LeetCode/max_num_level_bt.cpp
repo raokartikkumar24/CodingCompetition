c76efcae717b573829461ba42ac97fcbc716080e
raokartikkumar <krao24@gmail.com>
2020-07-21 17:38:42 +0530

    added queue logic.needs more work

--- LeetCode/max_num_level_bt.cpp
+++ LeetCode/max_num_level_bt.cpp
@@ -40,4 +40,54 @@ public:
         return re;
         
     }
+
+    /*
+    //Using queues. doesnt work correctly. Need to revisit
+    vector<int> largestValues(TreeNode* root) {
+        
+        if(root == nullptr) nullptr;
+        
+          vector<int> result;
+        std::map<int, vector<int>> levelMap;
+        
+        int level = 0;
+        std::queue<TreeNode*> qu;
+        qu.push(root);
+        vector<int> test;
+        test.push_back(root->val);
+        levelMap.insert({0, test});
+        
+        while(!qu.empty()) {
+            
+            TreeNode *temp = qu.front();
+            qu.pop();
+            vector<int> tree_values;
+            level++;
+            
+            if(temp->left != nullptr) {
+                qu.push(temp->left);
+                tree_values.push_back(temp->left->val);
+                
+            }
+            if(temp->right != nullptr) {
+                qu.push(temp->right);
+                tree_values.push_back(temp->right->val);
+            }
+            
+            levelMap.insert({level, tree_values});
+        }
+        
+        for(auto &it : levelMap) {
+           int max_val = 0;
+            for(auto &vec : it.second) {
+                max_val = max(max_val, vec);
+            }
+            result.push_back(max_val);
+            
+        }
+        
+        return result;
+        
+    }
+    */
 };
