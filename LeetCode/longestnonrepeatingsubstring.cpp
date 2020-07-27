6c12f7b71f7b67641e245be866e405df7177f022
raokartikkumar <krao24@gmail.com>
2020-07-27 18:17:21 +0530

    anotherone

--- LeetCode/longestnonrepeatingsubstring.cpp
+++ LeetCode/longestnonrepeatingsubstring.cpp
@@ -1,26 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    bool isStringUnique(string s, int start, int end) {
         
         unordered_set<char> hashMap;
         
+        for(int i = start ;  i < end; i++) {
+            if(hashMap.count(s[i]) > 0 ) return false;
+            hashMap.insert(s[i]);
+        }
+        return true;
+    }
+    
+    int lengthOfLongestSubstring(string s) {
+                
         int max_len = 0;
         
-        for(int i = 0; i < s.length(); i++) {
-            if(hashMap.count(s[i]) == 0) {
-                hashMap.insert(s[i]);
-            }    
-            else {
-                max_len = max(max_len, (int) hashMap.size());
-                hashMap.clear();
+        for(int i = 0; i < s.length(); i++) {      
+            for(int j = i +1 ; j <= s.length(); j++) {
+                if(isStringUnique(s, i, j)) {
+                    max_len = max(max_len, j - i);
+                }
             }
         }
-        
-        if(hashMap.size() != 0) {
-            max_len = max(max_len, (int) hashMap.size());
-        }
-        
         return max_len;
-        
     }
 };
+
+int main() {
+
+    Solution sol;
+
+    string inputString;
+    std::cin >> inputString;
+
+    int nlengthOfLongestSubstring = sol.lengthOfLongestSubstring(inputString);
+
+    std::cout << nlengthOfLongestSubstring << "\n";
+
+    return 0;
+}
