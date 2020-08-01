82b63d78aa65538866172b793ba1ba243aa6279c
raokartikkumar <krao24@gmail.com>
2020-08-01 22:30:04 +0530

    2 pointer method

--- LeetCode/reverseWords.cpp
+++ LeetCode/reverseWords.cpp
@@ -6,6 +6,33 @@
  using namespace std;
 
 
+string reverseWords2(string s) {
+
+	string result = "";
+	vector<string> allWords;
+	int len = s.length();
+	int  j = len -1;
+	int i = 0;
+	while( j > 0) {
+		i = j;
+		while(s[i] != ' ' && i >= 0) {
+			i--;
+		}
+		string substring = s.substr(i+1, j-i);
+		if(substring.length() > 0)
+			result += substring + ' ';
+		if( i >= 0) {	
+			j = i - 1;
+		}
+		else {
+			break;
+		}
+
+	}
+
+	return result.substr(0, result.length()-1);
+}
+
  string reverseWords(string s) {
 
  		int len = s.length();
@@ -46,8 +73,8 @@
  	string ins;
  	getline(cin, ins);
 
- 	string rev = reverseWords(ins);
-
+ 	//string rev = reverseWords(ins);
+ 	string rev = reverseWords2(ins);
  	cout << rev << "\n";
 
  	return 0;
