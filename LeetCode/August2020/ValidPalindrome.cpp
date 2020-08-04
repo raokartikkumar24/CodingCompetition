ef8dc4370109d66bda8308c3bb5f46f0eee750ea
raokartikkumar <krao24@gmail.com>
2020-08-04 11:38:49 +0530

    valid palindrom

---
+++ LeetCode/August2020/ValidPalindrome.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int len = s.length();
+        std::transform(s.begin(), s.end(), s.begin(),
+         [](unsigned char c){ return std::tolower(c); });
+
+        
+        string newString;
+        
+        for(int i = 0 ; i < len; i++) {
+            if(s[i] >= 97 && s[i] <= 122) {
+                newString += s[i];
+            }
+            else if(s[i] >= 48 && s[i] <= 57) {
+                newString += s[i];
+            }
+        }
+
+
+        len = newString.length();
+         
+        int i = 0, j = len-1;
+        while(i <= j) {
+            
+            if(newString[i] != newString[j]){
+                return false;
+            }
+            i++;j--;
+            
+        }
+        
+        return true;
+    }
+};
+
+
+int main()
+{
+
+    Solution sol;
+
+    string in;
+    getline(cin, in);
+    cout << in << "\n";
+
+    string output = (sol.isPalindrome(in) == 1) ? "Valid Palindrome" : "Invalid Palindrome";
+    cout <<  output << "\n";
+
+    return 0;
+
+}
