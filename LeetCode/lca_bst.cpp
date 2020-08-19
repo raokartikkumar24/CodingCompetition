1928260edb381111e21cef103a552aef44defa2e
raokartikkumar <krao24@gmail.com>
2020-08-19 22:13:08 +0530

    more

--- LeetCode/lca_bst.cpp
+++ LeetCode/lca_bst.cpp
@@ -24,4 +24,17 @@ public:
         return left != nullptr ? left : right;
         
     }
+
+    TreeNode* lowestCommonAncestorBST(TreeNode* root, TreeNode* p, TreeNode* q) {
+               
+        if(root->val > p->val && root->val > q->val) 
+            return lowestCommonAncestor(root->left, p, q);
+        
+        else if(root->val < p->val && root->val < q->val) 
+            return lowestCommonAncestor(root->right, p ,q);
+        
+        else 
+            return root;
+      
+    }
 };
